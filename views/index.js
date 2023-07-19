442ff2883bf6142167f5b3b6762a4c170c8e16c4
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2023-07-19 08:37:02 +0100

    var table name changed to viewTodosTable

--- views/index.js
+++ views/index.js
@@ -5,7 +5,7 @@
 var controller;
 
 // Variable to store HTML table object
-var table = document.getElementById("viewTodosTable");
+var viewTodosTable = document.getElementById("viewTodosTable");
 var data = {
     "id": 23
 };
@@ -36,7 +36,7 @@ function Todos() {
         let inputName = document.getElementById("todoNameInputBox").value;
         let inputDesc = document.getElementById("todoDescInputBox").value;
         postNewTodo(inputName, inputDesc);
-    }
+    };
     
     this.viewTodos = function() {
         
@@ -46,7 +46,7 @@ function Todos() {
         let numOfRows = 3;
         
         for(let i = 0; i < numOfRows; i++) {
-            let newRow = table.insertRow(table.rows.length);
+            let newRow = viewTodosTable.insertRow(viewTodosTable.rows.length);
             let cell1 = newRow.insertCell(0);
             let cell2 = newRow.insertCell(1);
             let cell3 = newRow.insertCell(2);
@@ -58,27 +58,27 @@ function Todos() {
             cell4.innerHTML = "<button onclick='controller.deleteTodo(this)'>Delete</button>";
             cell5.innerHTML = "<button onclick='controller.saveEdit(this)'>Save edit</button>";
         }
-    }
+    };
     
     this.saveEdit = function(clicked_object) {
         clicked_object.setAttribute("name", data.id);
         clicked_object.setAttribute("id", "s1");
         alert(clicked_object.getAttribute("name"));
         alert(clicked_object.getAttribute("id"));
-    }
+    };
     
     this.deleteTodo = function(clicked_object) {
         clicked_object.setAttribute("name", data.id);
         clicked_object.setAttribute("id", "d1");
         alert(clicked_object.getAttribute("name"));
         alert(clicked_object.getAttribute("id"));
-    }    
+    };    
     
     // HELPER FUNCTIONS
     
     function clearTable() {
-        while(table.rows.length > 0) {
-            table.deleteRow(-1);
+        while(viewTodosTable.rows.length > 0) {
+            viewTodosTable.deleteRow(-1);
         }       
     }
 }
